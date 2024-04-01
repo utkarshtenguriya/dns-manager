ec71cdec6934e7bcf121211495c46ce2a7770e80
Wasim Raja Middya <104504639+wasimmiddya@users.noreply.github.com>
2024-04-01 12:47:12 +0530

    fixed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,26 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-<<<<<<< HEAD
-    // proxy: {
-    //     '^/api/.*': {
-    //         target: 'https://dns-manager-n39a.onrender.com',
-    //         changeOrigin: true,
-    //         secure: true,
-    //     }
-    // }
-=======
-    proxy: {
-        '^/api/.*': {
-            target: 'https://dns-manager-n39a.onrender.com',
-            changeOrigin: true,
-            secure: false,
-        }
-    }
->>>>>>> 95635779c305f5094ef9688f58503eda8ce6715d
-},
-})
+  server: {},
+});
